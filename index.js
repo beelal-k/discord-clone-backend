83da107516c8e93b3cb349bd0bc563f460092999
bilal-kun <khawajabilal902@gmail.com>
2022-11-10 23:40:01 +0500

    backend template

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+    res.send('Hello from server!');
+
+})
+
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log('Server started!');
+
+})
